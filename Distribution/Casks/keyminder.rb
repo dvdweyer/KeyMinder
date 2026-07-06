2427dc2b710cf06c4eef7de1280bd8d9bdbb99ca
Donald van de Weyer <github.com@afaik.org>
2026-07-06 07:24:04 -0700

    chore: pre-deploy prep for v1.0.175

--- Distribution/Casks/keyminder.rb
+++ Distribution/Casks/keyminder.rb
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 cask "keyminder" do
-  version "1.0.173"
+  version "1.0.175"
   sha256 "c0c8d49a3545276e65436269c383ac18fbaca1f70ea90ad627cbb7a26d577bfc"
 
   url "https://keyminder.app/KeyMinder_#{version}.dmg"
