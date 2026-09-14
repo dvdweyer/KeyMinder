b22331b0cd273fc7ac909651a6eb90f43ff40fc1
Donald van de Weyer <github.com@afaik.org>
2026-09-14 10:00:26 +0200

    chore: release artifacts for v1.0.195 (appcast, cask)

--- Distribution/Casks/keyminder.rb
+++ Distribution/Casks/keyminder.rb
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 cask "keyminder" do
-  version "1.0.175"
-  sha256 "ffc0339469688249dfd0416e0979ca01fd95a720cfd055f3978114c48bac8d16"
+  version "1.0.195"
+  sha256 "f277ec2092c9e38de42603244c220c0cf3d719fd0e63a45d663b9eb0f8cf7241"
 
   url "https://keyminder.app/KeyMinder_#{version}.dmg"
   name "KeyMinder"
